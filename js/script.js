1b2b15993b9447a48fb9fad30ba68f436af82d0e
CEED87 <ceed210787@gmail.com>
2021-07-09 18:47:40 +0300

    add_nav_and_menu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const tabheader = document.querySelectorAll('.tabheader__item');
+
+
+
+// tabheader.forEach(elemnt => {
+//     elemnt.addEventListener('mouseover', () => {
+//         elemnt.classList.add('text__menu');
+//     });
+// });
+// console.log(tabheader);
