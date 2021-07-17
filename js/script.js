96d91cf6a1f030eb14499f1488257c87d39f7158
CEED87 <ceed210787@gmail.com>
2021-07-17 23:14:02 +0300

    add tab_menu in js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-const tabheader = document.querySelectorAll('.tabheader__item');
+const tabheaderItem = document.querySelector('.tabheader__items');
+const tabContent = document.querySelector('.tabcontent');
+
+
+// console.log(tabContent)
+tabheaderItem.addEventListener('click', (event) => {
+    const tab = event.target;
+    const actvTabe = tabheaderItem.querySelector('.active__item');
+    const imgTab = document.querySelectorAll('div[data-img]');
+     
+    if (tab.className === 'tabheader__item') {
+        actvTabe.classList.remove('active__item');
+        tab.classList.add('active__item');
+
+        imgTab.forEach(el => {
+            if (el.dataset.img === tab.dataset.tabe) {
+                let atrValue = el.firstElementChild.getAttribute('src');
+                let content = el.lastElementChild.textContent;
+                tabContent.firstElementChild.setAttribute('src',atrValue);
+                tabContent.lastElementChild.textContent = content;
+                // console.log();
+            }
+        });
+    } 
+});
 
 
 
-// tabheader.forEach(elemnt => {
-//     elemnt.addEventListener('mouseover', () => {
-//         elemnt.classList.add('text__menu');
-//     });
-// });
-// console.log(tabheader);
+
