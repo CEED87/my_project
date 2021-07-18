1772f24cee437f7821ba3d94c17bfc9dd44cc501
CEED87 <ceed210787@gmail.com>
2021-07-18 20:35:48 +0300

    add mini_slide

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+// Tabe_menu && slider
+
 const tabheaderItem = document.querySelector('.tabheader__items');
 const tabContent = document.querySelector('.tabcontent');
 
 
-// console.log(tabContent)
 tabheaderItem.addEventListener('click', (event) => {
     const tab = event.target;
     const actvTabe = tabheaderItem.querySelector('.active__item');
@@ -18,11 +19,42 @@ tabheaderItem.addEventListener('click', (event) => {
                 let content = el.lastElementChild.textContent;
                 tabContent.firstElementChild.setAttribute('src',atrValue);
                 tabContent.lastElementChild.textContent = content;
-                // console.log();
             }
         });
     } 
 });
+
+// Slider foto
+
+const arrows = document.querySelector('.offer__slider-counter');
+const arrowRight = document.querySelector('img[alt="next"]');
+const arrowLeft = document.querySelector('img[alt="prev"]');
+const imgSlide = document.querySelector('.mini').children;
+const offerSlide = document.querySelector('.offer__slide').firstElementChild;
+const current = arrows.querySelector('#current');
+
+let count = 1;
+offerSlide.src = imgSlide[count-1].src;
+
+
+arrowRight.addEventListener('click', () => {
+    if (current.textContent <= imgSlide.length-1) {
+        current.textContent = `0${++count}`;
+        offerSlide.src = imgSlide[count-1].src;
+    } 
+});
+    
+arrowLeft.addEventListener('click', () => {
+    if (current.textContent > 1) {
+        current.textContent = `0${--count}`;
+        offerSlide.src = imgSlide[count-1].src;
+        console.log(current)
+    }
+});
+
+
+
+
 
 
 
