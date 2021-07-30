40b4fe3cb312e034fa6b388b2a98c6efff81a406
CEED87 <ceed210787@gmail.com>
2021-07-30 18:47:02 +0300

    add local_sever

--- js/feedback.js
+++ js/feedback.js
@@ -1,34 +1,55 @@
 // Model feedback
 
 const modalTrigger = document.querySelector('button[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]');
+        modal = document.querySelector('[data-user]'),
+        modalCloseBtn = document.querySelectorAll('[data-close]'),
+        addCard = document.querySelector('[data-card]'),
+        addCardBtn = document.querySelector('[href="#"]')
 
 
-const closeModal = () => {
-        modal.classList.add('hide');
-        modal.classList.remove('show');
+const closeModal = (wind) => {
+        wind.classList.add('hide');
+        wind.classList.remove('show');
         document.body.style.overflow = '';
 }
 
-const openModal = () => {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
+const openModal = (wind) => {
+        wind.classList.add('show');
+        wind.classList.remove('hide');
         document.body.style.overflow = 'hidden';
 };
 
-modalTrigger.addEventListener('click', openModal);
-modalCloseBtn.addEventListener('click', closeModal);
+modalTrigger.addEventListener('click', () => {
+        openModal(modal);
+});
+modalCloseBtn.forEach((el) => {
+        el.addEventListener('click', () => {
+                closeModal(modal);
+                closeModal(addCard);
+        })
+});
 
 modal.addEventListener('click', (e) => {
         if (e.target === modal) {
-                closeModal();
+                closeModal(modal);
         }
 });
 
 document.addEventListener('keydown', (e) => {
-        if (e.code === "Escape" && modal.classList.contains('show')) {
-                closeModal();
+        if (e.code === "Escape" && modal.classList.contains('show') || addCard.classList.contains('show')) {
+                closeModal(modal);
+                closeModal(addCard);
+        }
+});
+
+addCardBtn.addEventListener('click', () => {
+        openModal(addCard);
+});
+
+
+addCard.addEventListener('click', (e) => {
+        if (e.target === addCard) {
+                closeModal(addCard);
         }
 });
 
@@ -41,11 +62,11 @@ const phone = document.querySelector('input[name="phone"]');
 order.addEventListener('click', (event) => {
         event.preventDefault();
         if (event.target.className === 'btn btn_dark btn_min') {
-                console.log(firstName.value ,phone.value);
+
                 // console.log(firstName.value)
                 // console.log(phone.value)
                 firstName.value = '';
                 phone.value = '';
-                
+                console.log(firstName.value, phone.value);
         }
 });
--- js/server_info.js
+++ js/server_info.js
@@ -1,1 +1,32 @@
-console.log('dfsf')
+const container = document.querySelector('div[data-cards]');
+
+const getCards = () => {
+    fetch('http://localhost:3000/cards').then(res => {
+        return res.json();
+    }).then(data => {
+        let count = 0;
+        data.forEach(card => {
+            count++;
+            if (count <= 3) {
+                container.innerHTML += `
+                                    <div class="menu__item">
+                                    <img src="${card.img}" alt="fitnes">
+                                    <h3 class="menu__item-subtitle">${card.menu}</h3>
+                                    <div class="menu__item-descr">${card.text}</div>
+                                    <div class="menu__item-divider"></div>
+                                    <div class="menu__item-price">
+                                        <div class="menu__item-cost">Цена:</div>
+                                        <div class="menu__item-total"><span>${card.price}</span> руб/день</div>
+                                    </div>
+                                </div>
+                                    `;
+            }
+            
+        });
+    });
+};
+
+getCards();
+
+
+//   console.log(card)
